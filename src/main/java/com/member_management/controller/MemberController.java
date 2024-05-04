f8d33e5ba1d727aade9e2d25ff7adb6ab6f39ece
Nguyen Duc Thanh <thanh.nguyen@beetech.com>
2024-05-04 23:19:56 +0700

    handle show profile

--- src/main/java/com/member_management/controller/MemberController.java
+++ src/main/java/com/member_management/controller/MemberController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -110,5 +111,14 @@ public class MemberController {
             }
         }
         return "redirect:/change-password";
+    }
+
+    @GetMapping("/home")
+    public String showHomePage(Model model,
+            HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        _Member loggedInMember = (_Member) session.getAttribute("loggedInMember");
+        model.addAttribute("loggedInMember", loggedInMember);
+        return "home";
     }
 }
