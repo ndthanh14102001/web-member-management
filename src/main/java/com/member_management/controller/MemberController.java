1f402b50ddcdc66451f4fbc6a8019b44b2727e18
02Trong <nguyenquoctrong2706@gmail.com>
2024-05-04 15:37:25 +0700

    Update MemberController.java

--- src/main/java/com/member_management/controller/MemberController.java
+++ src/main/java/com/member_management/controller/MemberController.java
@@ -83,4 +83,28 @@ public class MemberController {
             return "redirect:/signup";
         }
     }
+    public String changePassword(@RequestParam("maTV") String id,
+        @RequestParam("currentPassword") String currentPassword,
+        @RequestParam("newPassword") String newPassword,
+        RedirectAttributes redirectAttributes,
+        HttpServletRequest request) {
+    HttpSession session = request.getSession();
+    _Member loggedInMember = (_Member) session.getAttribute("loggedInMember");
+    
+    if (loggedInMember != null && loggedInMember.getMaTV().equals(id)) {
+        if (loggedInMember.getPassword().equals(currentPassword)) {
+            loggedInMember.setPassword(newPassword);
+            memberService.updateMember(loggedInMember);
+            
+            redirectAttributes.addFlashAttribute("successMessage", "Mật khẩu đã được thay đổi thành công!");
+            return "redirect:/home";
+        } else {
+            redirectAttributes.addFlashAttribute("errorMessage", "Mật khẩu hiện tại không đúng!");
+            return "redirect:/changepassword";
+        }
+    } else {
+        redirectAttributes.addFlashAttribute("errorMessage", "Bạn không được phép thay đổi mật khẩu cho người dùng này!");
+        return "redirect:/home";
+    }
+}
 }
