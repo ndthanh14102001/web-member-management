f632220b5a71e14f7eb23c8cd9fddfe8ad33c809
02Trong <nguyenquoctrong2706@gmail.com>
2024-05-04 11:59:19 +0700

    Update ChangePasswordController.java

--- src/main/java/com/member_management/controller/ChangePasswordController.java
+++ src/main/java/com/member_management/controller/ChangePasswordController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+@Controller
 public class ChangePassword{
 @PostMapping("/changepassword")
 public String changePassword(@RequestParam("maTV") String id,
